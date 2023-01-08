a658ca4cf48108a3f01be0545a0441d6b21c5e9e
1Sayd <person.tak@gmail.com>
2023-01-08 20:06:19 +0200

    Add RESTful api usage

--- App.js
+++ App.js
@@ -7,15 +7,20 @@
  */
 import 'react-native-gesture-handler';
 
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import type {Node} from 'react';
 import {
+  ActivityIndicator,
+  Alert,
   Button,
+  FlatList,
   SafeAreaView,
   ScrollView,
   StatusBar,
   StyleSheet,
   Text,
+  TextInput,
+  TouchableHighlight,
   useColorScheme,
   View,
 } from 'react-native';
@@ -31,6 +36,41 @@ import {
 import {NavigationContainer} from '@react-navigation/native';
 import {createDrawerNavigator} from '@react-navigation/drawer';
 
+// Components
+const Note = () => {
+  const [text, setText] = useState('');
+  return (
+    <View
+      style={{
+        margin: 30,
+        padding: 10,
+        backgroundColor: 'cadetblue',
+      }}>
+      <TextInput style={{backgroundColor: 'cornsilk'}} multiline={true} />
+
+      <TextInput
+        style={{marginTop: 20, marginBottom: 40, backgroundColor: 'burlywood'}}
+        multiline={true}
+      />
+
+      <Button
+        title="Press me"
+        color="#841584"
+        onPress={() => Alert.alert('Simple Button pressed')}
+      />
+
+      {/*<Text style={{padding: 10, fontSize: 42}}>
+        {text
+          .split(' ')
+          .map(word => word && '🍕')
+          .join(' ')}
+      </Text>*/}
+    </View>
+  );
+};
+
+// Pages
+// Page 1
 function ToDoScreen() {
   return (
     <View>
@@ -40,19 +80,60 @@ function ToDoScreen() {
   );
 }
 
+// Page 2
 function NotesScreen() {
   return (
     <View>
       <Text>Notes Page</Text>
+
+      <Note />
+      <Note />
     </View>
   );
 }
 
+// Page 3
 function MotivationalScreen() {
+  let [quote, setQuote] = React.useState('');
+  let [source, setSource] = React.useState('');
+
+  const fetchApiCall = () => {
+    fetch('https://quotes15.p.rapidapi.com/quotes/random/?language_code=en', {
+      method: 'GET',
+      headers: {
+        'x-rapidapi-host': 'quotes15.p.rapidapi.com',
+        'x-rapidapi-key': '3434f4eab0mshbd6b6ba4ea777aep19d57ejsnadde1bbd7df6',
+      },
+    })
+      .then(response => response.json())
+      .then(response => {
+        setQuote(response.content);
+        setSource(response.originator.name);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  };
   return (
-    <View>
-      <Text>Motivational Page</Text>
-    </View>
+    <TouchableHighlight onPress={fetchApiCall}>
+      <View>
+        <TouchableHighlight onPress={fetchApiCall}>
+          <View
+            style={{
+              padding: 10,
+              marginVertical: 15,
+              backgroundColor: '#0645AD',
+            }}>
+            <Text style={{color: '#fff'}}>Use Fetch API</Text>
+          </View>
+        </TouchableHighlight>
+        <View>
+          <Text>{quote}</Text>
+          <Text>{source}</Text>
+        </View>
+        <StatusBar style="auto" />
+      </View>
+    </TouchableHighlight>
   );
 }
 const Drawer = createDrawerNavigator();
